b3de722bb0166d64a50f310455a80cbb6ea7498e
arun2352000 <poorvaja10@gmail.com>
2024-01-02 00:05:33 +0530

    update of day 5 on readme file

---
+++ Day 5 task/Readme.txt
@@ -0,0 +1,4 @@
+the below day 5 task contains a js file
+this contains two parts 
+part 1 contains my own resume data in json format
+part 2 contains json iterate over all for loop (for, for in, for of, forEach) 
--- Day 5 task/Task1.js
+++ Day 5 task/Task1.js
@@ -1,29 +1,83 @@
-// resume data using json
-
-var resume = {
-    "name": "arunachalam",
-    "education": {
-        "schooling": {
-            
-                "name": "santhome hig.sec.school",
-                "cgp": "'7.5'",
-                "std": "HSC",      
-            
-                "name": "santhome hig.sec.school",
-                "cgp": "'7.5'",
-                "std": "SSC"     
-        } ,
-        "college": 'AM jain'
+let myResume = {
+  basics: {
+    name: "Arunachalam P",
+    email: "arunachalam2352000@gamil.com",
+    phone: 9445805362,
+    degree: "M.Sc",
+    location: {
+      address: "pblock door no 4 cid police quarters",
+      postalCode: 600004,
+      city: "Chennai",
+      state: "Tamilnadu",
+      country: "India",
     },
+    profiles: [
+      {
+        website: "www.linkedin.com/in/arunachalam2352000",
+        github: "https://github.com/arun2352000",
+      },
+    ],
+  },
 
-    "contactDetails": {
-        "email": 'arunachalam2352000@gmail.com',
-        "phone": "9445805362",
-        "pincode":"600004"
+  education: [
+    {
+      institution: "Sri krishna college of arts and science",
+      department: "electronics and communication",
+      studyType: "fulltime",
+      "batch start year": 2020,
+      "batch end year": 2022,
+      gpa: 8.6,
     },
-    "skills": ['C', 'C++', 'Js', 'Python', 'react']
+  ],
+  skills: [
+    {
+      name: "python,javascript",
+      level: "beginer",
+      project: "home automation",
+    },
+  ],
+  languages: [
+    {
+      language: "Tamil,Enlish",
+    },
+  ],
+  interests: [
+    {
+      name: "web development, automation and iot developer",
+    },
+  ],
+};
+
+// console.log(myResume);
+// const obj = JSON.parse(myResume);
+var obj1 = JSON.stringify(myResume);
+// console.log(obj1);
+var obj = JSON.parse(obj1);
+
+// for loop
+console.log("\n for loop");
+let keys = Object.keys(obj);
+for (let i = 0; i <= keys.length; i++) {
+  console.log(keys[i], obj[keys[i]]);
 }
-const obj = JSON.parse(resume);
-// console.log(JSON.stringify(resume));
-// console.log(JSON.parse(resume));
-console.log(resume.skills);
+
+// for in loop
+console.log("\n for in loop");
+for (let key in obj) {
+  
+  console.log(key, obj[key]);
+  
+}
+
+// for of loop
+console.log("\n for of loop");
+const obj2 = obj;
+for (const [key, value] of Object.entries(obj2)) {
+  console.log(key, value);
+}
+
+// for each loop
+console.log("\n for each loop");
+let res = Object.values(obj);
+
+res.forEach((num) => console.log(num));
